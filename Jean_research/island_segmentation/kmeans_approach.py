9fd7c88a0edfd914968887d76c8f9140b3f7b224
jyaacoub <jyaacoub21@gmail.com>
2022-01-17 16:37:26 -0500

    attempted clustering approaches for image segmentation
    Although clustering is significantly better than threshold-based approaches, it still struggles and causes some data loss in the pieces.
    
    Clustering is very good at blending out the noisy background compared to threshold methods that just uses grayscale..

--- Jean_research/island_segmentation/kmeans_approach.py
+++ Jean_research/island_segmentation/kmeans_approach.py
@@ -1,21 +1,17 @@
-# from this https://github.com/omadson/fuzzy-c-means 
+# from this: https://towardsdatascience.com/image-segmentation-with-clustering-b4bbc98f2ee6
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
-from fcmeans import FCM
-
 img = cv2.imread('our_p.png', cv2.IMREAD_UNCHANGED)
 vectorized = img.reshape((-1,3))
-
-fcm = FCM(n_clusters=2, m=2)
-fcm.fit(vectorized)
-centers = np.uint8(fcm.centers)
-labels = centers[fcm.predict(vectorized)]
-
-segmented_image = labels.reshape((img.shape))
+kmeans = KMeans(n_clusters=2, n_init=10, max_iter=1000).fit(vectorized)
+centers = np.uint8(kmeans.cluster_centers_)
+segmented_data = centers[kmeans.labels_.flatten()]
+ 
+segmented_image = segmented_data.reshape((img.shape))
 plt.imshow(segmented_image)
 plt.suptitle("segmented")
 plt.figure()
