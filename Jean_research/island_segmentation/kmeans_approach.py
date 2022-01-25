5a30e9e5d5bd07389db21cb045a6e75bb6d7fe33
jyaacoub <jyaacoub21@gmail.com>
2022-01-25 10:09:21 -0500

    kmeans works the best with ideal case for segmenting
    ideal case = dark puzzle pieces with white contrasting background and even lighting

--- Jean_research/island_segmentation/kmeans_approach.py
+++ Jean_research/island_segmentation/kmeans_approach.py
@@ -1,20 +1,57 @@
 # from this: https://towardsdatascience.com/image-segmentation-with-clustering-b4bbc98f2ee6
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+from scipy.ndimage import filters
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
-img = cv2.imread('mu_p.jpg', cv2.IMREAD_UNCHANGED)
+img = cv2.imread('C:\\Users\\Jean\\Desktop\\Undergrad\\2022 Winter\\CISC 499\\PowerfulPuzzling\\dataset\\starry_night\\edge_case.JPG',
+        cv2.IMREAD_UNCHANGED)
 vectorized = img.reshape((-1,3))
-kmeans = KMeans(n_clusters=2, n_init=10, max_iter=1000).fit(vectorized)
+kmeans = KMeans(n_clusters=2, n_init=10).fit(vectorized)
 centers = np.uint8(kmeans.cluster_centers_)
 segmented_data = centers[kmeans.labels_.flatten()]
  
 segmented_image = segmented_data.reshape((img.shape))
-plt.imshow(segmented_image)
+plt.imshow(segmented_image, cmap='gray')
 plt.suptitle("segmented")
 plt.figure()
 
-plt.imshow(img)
+img = cv2.cvtColor(segmented_image, cv2.COLOR_RGBA2GRAY) # grayscaling
+
+def find_cntrs(img, blur_amt=None):
+    if blur_amt:
+        blur = cv2.GaussianBlur(img, (blur_amt, blur_amt), 1)
+    cntrs, _ = cv2.findContours(blur, 0, 1)
+    srt = sorted([[cnt.shape[0], i] for i, cnt in enumerate(cntrs)], reverse=True)[:15]
+    biggest = [cntrs[s[1]] for s in srt]  
+    fill = cv2.drawContours(np.zeros(blur.shape[:2]), biggest, -1, 255, thickness=cv2.FILLED)
+    return fill
+
+def smooth_edges(img, thickness=3):
+    smooth = filters.median_filter(img.astype('uint8'), size=10) 
+    trim_contours, _ = cv2.findContours(smooth, 0, 1) 
+    cv2.drawContours(smooth, trim_contours, -1, color=0, thickness=thickness)
+    return smooth
+
+c=-10
+blocksize=5
+thr = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, 
+                            cv2.THRESH_BINARY, blocksize, c)
+
+plt.imshow(img, cmap='gray')
+plt.suptitle("grayscaled")
+plt.figure()
+
+fill = find_cntrs(thr, 5)
+
+plt.imshow(fill, cmap="gray")
+plt.suptitle("block: {}, c: {}".format(blocksize, c))
+
+smoothed = smooth_edges(fill, 5)
+
+plt.imshow(smoothed, cmap='gray')
+plt.suptitle("smoothed")
+plt.figure()
 plt.show()
