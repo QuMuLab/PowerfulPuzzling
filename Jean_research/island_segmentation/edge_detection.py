8ef9891e7d42492b7a6fc7a2726f06683c26aa53
jyaacoub <jyaacoub21@gmail.com>
2022-01-25 10:09:21 -0500

    kmeans works the best with ideal case for segmenting
    ideal case = dark puzzle pieces with white contrasting background and even lighting

--- Jean_research/island_segmentation/edge_detection.py
+++ Jean_research/island_segmentation/edge_detection.py
@@ -5,20 +5,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-img = np.array(Image.open('Mu_p.jpg').convert('RGBA'))
+img = np.array(Image.open('C:\\Users\\Jean\\Desktop\\Undergrad\\2022 Winter\\CISC 499\\PowerfulPuzzling\\dataset\\starry_night\\edge_case.JPG'
+        ).convert('RGBA'))
 img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY) # grayscaling
 
-c=-3
-blocksize=3
-
+c=-10
+blocksize=5
 thr = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, 
                             cv2.THRESH_BINARY, blocksize, c)
-# thr = cv2.GaussianBlur(thr, (blocksize,blocksize), 1)
-cntrs, _ = cv2.findContours(thr, 0, 1)
+blur = cv2.GaussianBlur(thr, (5,5), 1)
+cntrs, _ = cv2.findContours(blur, 0, 1)
 srt = sorted([[cnt.shape[0], i] for i, cnt in enumerate(cntrs)], reverse=True)[:15]
 biggest = [cntrs[s[1]] for s in srt]  
 fill = cv2.drawContours(np.zeros(img.shape[:2]), biggest, -1, 255, thickness=cv2.FILLED)
 
+smooth = filters.median_filter(fill.astype('uint8'), size=10) 
+trim_contours, _ = cv2.findContours(smooth, 0, 1) 
+cv2.drawContours(smooth, trim_contours, -1, color=0, thickness=3) 
+
+plt.imshow(thr, cmap="gray")
+plt.suptitle("thresh")
+
+plt.figure()
 plt.imshow(fill, cmap="gray")
 plt.suptitle("block: {}, c: {}".format(blocksize, c))
+
+plt.figure()
+plt.imshow(smooth, cmap="gray")
+plt.suptitle("smooth")
 plt.show()
