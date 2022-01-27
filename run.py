460a1afe2ebec96eca33465a4d8d722ac825f465
jyaacoub <jyaacoub21@gmail.com>
2022-01-27 17:16:34 -0500

    Fix pathing issue and added new Matcher class
    - Added outline for the Matcher that will determine how likely two pieces are matching.
    - Note: added temporary file in segmentation package for the segmentation and the extraction of the borders

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 """This is where we will call the functions from the powerful pluzzling algo"""
 from src.app import get_hint, complete_puzzle
+from src.border_matching import Matcher
+from src.segmentation.FIXME import get_image_and_border
 
+# %%
+img, borders = get_image_and_border('dataset\\starry_night\\edge_case.jpg')
+print(img.shape)
+Matcher(img)
