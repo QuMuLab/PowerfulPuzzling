92885466c2c1ba746fbe3aebee8776bf3f8b08a2
jyaacoub <jyaacoub21@gmail.com>
2022-02-09 17:21:43 -0500

    Added more stub functions for matcher
    - High level classification of a border segment shape with __determine_shape() (uses polyfit)
    - lower-level matching with __match_distance that uses DTW

--- src/border_matching/matcher.py
+++ src/border_matching/matcher.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 
 class Matcher:
@@ -26,18 +27,44 @@ class Matcher:
             for j in range(i+1, n):
                 probable_matches[i,j] = self.match_likelihood(contours[i], contours[j])                
 
-    def match_likelihood(self, border1, border2) -> float:
-        """Determines how likely it is that two borders are matching
-
+    def __match_distance(self, seg1:np.array, seg2:np.array, step_pattern="symmetric2") -> float:
+        """
+        This uses Dynamic Time Warping (DTW) and returns the normalized distance 
+        value, to determine how well two segments fit with each other.
+        
         Args:
-            border1 ([type]): [description]
-            border2 ([type]): [description]
+            seg1 (np.array): The border segment.
+            seg2 (np.array): The border segment to match with.
+            step_pattern (str, optional): The step pattern to use when applying DTW 
+                (see dtw-python docs for more info). Defaults to "symmetric2".
 
         Returns:
-            float: MSE for how closely the borders match
+            float: Distance value from DTW.
         """
         pass
+    
+    def __determine_shape(border_segment:np.array) -> tuple(int, [float]):
+        """
+        High-level determination of the shape of a border segment using np.polyfit.
+        Possible shapes are:
+            - Concave (\\\/)
+            - Convex (/\\\)
+            - Linear (--)
+        
+        This is determined by looking at the constant in a 2nd order polygon fitted to the 
+        points and seeing if it is - (convex), + (concave), or ~0 (linear).
 
+        Args:
+            border_segment ([type]): The segment to classify.
+            
+        Returns:
+            (int, [float]): The coefficeints for the polygon 
+        
+        References:
+            - np.polyFit: https://numpy.org/doc/stable/reference/generated/numpy.polyfit.html#numpy-polyfit
+        """
+        
+    
     def __get_line(self) -> np.array:
         pass
     def __get_MSE(self) -> float:
