56201649698225045dc8cc169846aa4d37dfe05b
jyaacoub <jyaacoub21@gmail.com>
2022-02-10 11:09:51 -0500

    Created rotate_points function for unrolling
    rotating the points then observing from one side to unroll

--- src/border_matching/matcher.py
+++ src/border_matching/matcher.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Iterable
 import numpy as np
 from dtw import dtw
+from sklearn.utils import compute_sample_weight
 
 class Matcher:
     def __init__(self, original_image: np.array) -> None:
@@ -105,5 +106,40 @@ class Matcher:
             
         return out
     
-    def __get_MSE(self) -> float:
-        pass
+    
+    def __rotate_points(points:np.array, n=1, inplace=True) -> np.array:
+        """
+        Rotates 2D lists of points by n degrees using a rotation matrix.
+
+        Args:
+            points (np.array): list of 2D points to be rotated.
+            n (int, optional): How many degrees to rotate points by. Defaults to 1.
+            inplace (bool, optional): Whether or not to rotate around points center 
+                    to maintain position instead of around origin. Defaults to True.
+
+        Returns:
+            np.array: The new list of points rotated by n degrees
+            
+        Reference:
+            https://en.wikipedia.org/wiki/Rotation_matrix
+        """
+        # creating rotation matrix
+        rad = np.radians(n) # needed because np.cos only takes radians as inputs
+        cos_0 = np.cos(rad)
+        sin_0 = np.sin(rad)
+        rot_mat = np.array([[cos_0, -sin_0],
+                            [sin_0,  cos_0]])
+        
+        if inplace:
+            # getting centre point
+            max_x, min_x = max(points[:,0]), min(points[:,0])
+            max_y, min_y = max(points[:,1]), min(points[:,1])
+            center = [min_x + ((max_x - min_x) / 2),
+                      min_y + ((max_y - min_y) / 2)]
+            
+            centered = points - center # translate to origin
+            rotated = centered@rot_mat # performing rotation
+            rotated += center # translate back
+        else:
+            rotated = points@rot_mat
+        return rotated
