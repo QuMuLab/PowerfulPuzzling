f563a39ea77f22a6d2d1da3b05ace861ddeef70a
jyaacoub <jyaacoub21@gmail.com>
2022-02-10 09:57:53 -0500

    completed high-level shape determination function
    In matcher class: __determine_shape and __get_line are filled out.

--- src/border_matching/matcher.py
+++ src/border_matching/matcher.py
@@ -1,5 +1,6 @@
-from typing import Tuple
+from typing import Tuple, Iterable
 import numpy as np
+from dtw import dtw
 
 class Matcher:
     def __init__(self, original_image: np.array) -> None:
@@ -11,7 +12,7 @@ class Matcher:
         """
         self.puzzle = original_image
         
-    def find_matching(self, contours: np.array) -> tuple[int, int, float, float]:
+    def find_matching(self, contours: np.array) -> Tuple[int, int, float, float]:
         """Finds two pieces that match with each other given their borders.
 
         Args:
@@ -39,33 +40,70 @@ class Matcher:
                 (see dtw-python docs for more info). Defaults to "symmetric2".
 
         Returns:
-            float: Distance value from DTW.
+            float: Normalized distance value from DTW.
         """
         pass
     
-    def __determine_shape(border_segment:np.array) -> tuple(int, [float]):
+    def __determine_shape(border_segment:np.array, cutoff=0.01) -> Tuple[int, Tuple[float]]:
         """
-        High-level determination of the shape of a border segment using np.polyfit.
-        Possible shapes are:
-            - Concave (\\\/)
-            - Convex (/\\\)
-            - Linear (--)
+        High-level determination of the shape of an unrolled border segment using np.polyfit.
+        Possible shapes are:            
+            0 - Concave (\\\/)\n
+            1 - Convex (/\\\)\n
+            2 - Linear (--)
         
-        This is determined by looking at the constant in a 2nd order polygon fitted to the 
+        This is determined by looking at the constant in a 2nd order polynomial fitted to the 
         points and seeing if it is - (convex), + (concave), or ~0 (linear).
 
         Args:
-            border_segment ([type]): The segment to classify.
+            border_segment (np.array): The unrolled border segment shape must be (x,) 
+                    where x is the number of points.
+            cutoff (float, optional): The cutoff for classification (e.g.: a cutoff of 1 
+                    means linear is anything between -1 and 1 coeff). Defaults to 0.0 
+                    (no linear aspect).                    
             
         Returns:
-            (int, [float]): The coefficeints for the polygon 
+            Tuple[int, Tuple[float]]: The determined shape (0-2) and the coefficents for the 
+                    polynomial.
         
         References:
-            - np.polyFit: https://numpy.org/doc/stable/reference/generated/numpy.polyfit.html#numpy-polyfit
+            - np.polyFit: https://numpy.org/doc/stable/reference/generated/numpy.polyfit.html
         """
+        # poly is a 3-tuple of the coeffs a,b, and c: (a*x^2 + b*x + c)
+        poly = np.polyfit(x=border_segment, y=list(range(border_segment.shape[0]), deg=2))
+             
+        if poly[0] > cutoff:
+            shape = 0
+        elif poly[0] < cutoff:
+            shape = 1
+        else:
+            shape = 2
         
+        return shape, poly
     
-    def __get_line(self) -> np.array:
-        pass
+    def __get_line(self, coeff:Tuple[float], a:int, b:int, steps=1) -> np.array:
+        """
+        Returns a list of points representing the polynomial function by the coefficients 
+        passed in.
+
+        Args:
+            coeff (Tuple[float]): The coefficients of the polynomial.
+            a (int): Starting range.
+            b (int): End range.
+            steps (int, optional): Steps for iterating through the range.
+
+        Returns:
+            np.array: The list of values from a to b outputted by the polynomial function.
+        """
+        out = np.array([])
+        order = len(coeff) - 1 # the polynomial order
+        for x in range(a, b, steps):
+            y = 0
+            for i, c in enumerate(coeff):
+                y += c*x**(order - i)
+            out.append(y)
+            
+        return out
+    
     def __get_MSE(self) -> float:
         pass
